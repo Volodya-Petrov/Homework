40ca2f04acb7478fac64bfc043bfb1a1cffe1d03
StenDarr <89377379140@mail.ru>
2020-10-25 19:01:34 +0300

    stack adds

--- Stack/Stack/Stack.h
+++ Stack/Stack/Stack.h
@@ -2,16 +2,22 @@
 
 #include <stdbool.h>
 
+//структура стека, имеет одно поле, а именно ссылку на голову стека
 struct Stack;
 
+//функция для выделения памяти на стек
 struct Stack* createStack(void);
 
+//Функция для получения значения последнего элемента в стеке
 int pop(struct Stack* stack);
 
+// добавление элемента в стек
 void push(struct Stack* stack, int value);
 
+// удаление стека
 void deleteStack(struct Stack** stack);
 
+// проверка на пустоту стека
 bool isEmpty(struct Stack* stack);
 
 
